1ca0b3761e078fcf6274d521d387d43d40b688ac
adieb <mrizqiamiruladieb@gmail.com>
2025-03-19 17:20:37 +0700

    feat: declare the object

--- src/Stopwatch.java
+++ src/Stopwatch.java
@@ -1,4 +1,47 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class Stopwatch {
+
+public class Stopwatch implements ActionListener {
+
+    //declare
+    JFrame frame = new JFrame();
+    JButton startButton = new JButton("START");
+    JButton resetButton = new JButton("RESET");
+    JLabel timeLabel = new JLabel();
+    int elapsedTime = 0;
+    int seconds = 0;
+    int minutes = 0;
+    int hours = 0;
+    boolean started = false;
+    String seconds_string = String.format("%02d", seconds);
+    String minutes_string = String.format("%02d", minutes);
+    String hours_string = String.format("%02d", hours);
+    
+    //construct
+    Stopwatch() {
+        
+        
+        
+    }
+    
+    //method
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    
+    void start() {
+        
+    }
+    
+    void stop() {
+        
+    }
+    
+    void reset() {
+        
+    }
     
 }
