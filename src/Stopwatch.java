7c684a8116889f2447a53c8d640686d67272b197
adieb <mrizqiamiruladieb@gmail.com>
2025-03-19 17:26:59 +0700

    feat: setup time label

--- src/Stopwatch.java
+++ src/Stopwatch.java
@@ -22,6 +22,15 @@ public class Stopwatch implements ActionListener {
     //construct
     Stopwatch() {
         
+        timeLabel.setText(hours_string + ":" + minutes_string + ":" + seconds_string);
+        timeLabel.setBounds(100, 100, 200, 100);
+        timeLabel.setFont(new Font("Verdana", Font.PLAIN, 35));
+        timeLabel.setBorder(BorderFactory.createBevelBorder(1));
+        timeLabel.setOpaque(true);
+        timeLabel.setHorizontalAlignment(JTextField.CENTER);
+        
+        frame.add(timeLabel);
+        
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420, 420);
         frame.setLayout(null);
